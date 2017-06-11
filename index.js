98770220c4b840a66ef4f984e069280163f41b7d
Bill Sykes <wsykes79@gmail.com>
2017-06-10 22:57:43 -0400

    Add Voice Insights tracking

--- index.js
+++ index.js
@@ -4,35 +4,56 @@ const facts = require('./facts.json');
 // Your app id (appears near the top of the page in the Alexa Developer Console).
 // We will pass this into our script via an environment variable.
 const MY_APP_ID = process.env.APP_ID;
+const VoiceLabs = require('voicelabs')('<App ID from the Voice Insights Dashboard>');
+const track = (session, name, slots, text, callback) => VoiceLabs.track(session, name, slots, text, callback);
 
 // Define some handlers
 const handlers = {
 
   // A catch all handler when we don't understand the request
   Unhandled: function() {
-    this.emit(':ask', 'Sorry, I\'m not sure what you mean.');
+    const text = 'Sorry, I\'m not sure what you mean.';
+
+    track(this.event.session, 'Unhandled', null, text, () => {
+      this.emit(':ask', text);
+    });
   },
 
   // A handler to get a city fact.
   GetCityFactIntent: function() {
+    const intent = this.event.request.intent;
     const index = Math.floor(Math.random() * facts.length);
+    const text = facts[index];
 
-    this.emit(':tell', facts[index]);
+    track(this.event.session, intent.name, intent.slots, text, () => {
+      this.emit(':tell', text);
+    });
   },
 
   'AMAZON.HelpIntent': function() {
+    const intent = this.event.request.intent;
     const text = 'You can say tell me a fact, or, you can say exit... What can I help you with?',
           reprompt = 'What can I help you with?';
 
-    this.emit(':ask', text, reprompt);
+    track(this.event.session, intent.name, intent.slots, text, () => {
+      this.emit(':ask', text, reprompt);
+    });
   },
 
   'AMAZON.CancelIntent': function() {
-    this.emit(':tell', 'Goodbye!');
+    const intent = this.event.request.intent;
+
+    track(this.event.session, intent.name, intent.slots, text, () => {
+      this.emit(':tell', 'Goodbye!');
+    });
   },
 
   'AMAZON.StopIntent': function() {
-    this.emit(':tell', 'Goodbye!');
+    const intent = this.event.request.intent;
+
+    track(this.event.session, intent.name, intent.slots, text, () => {
+      this.emit(':tell', 'Goodbye!');
+    });
   }
 };
 
